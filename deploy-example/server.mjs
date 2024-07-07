ea6a02bb688a17f0fa7368223b91602fc680f0c2
신석진( Seokjin Shin) <ssj9685@naver.com>
2024-07-07 18:49:50 +0900

    try catch

--- deploy-example/server.mjs
+++ deploy-example/server.mjs
@@ -11,26 +11,30 @@ const handle = app.getRequestHandler();
 app
   .prepare()
   .then(() => {
-    createServer(
-      {
-        key: fs.readFileSync(
-          "/etc/letsencrypt/live/https-test.p-e.kr/privkey.pem"
-        ),
-        cert: fs.readFileSync(
-          "/etc/letsencrypt/live/https-test.p-e.kr/cert.pem"
-        ),
-      },
-      async (req, res) => {
-        try {
-          await handle(req, res);
-        } catch (err) {
-          console.error("Error occurred handling", req.url, err);
-          res.statusCode = 500;
-          res.end("internal server error");
+    try {
+      createServer(
+        {
+          key: fs.readFileSync(
+            "/etc/letsencrypt/live/https-test.p-e.kr/privkey.pem"
+          ),
+          cert: fs.readFileSync(
+            "/etc/letsencrypt/live/https-test.p-e.kr/cert.pem"
+          ),
+        },
+        async (req, res) => {
+          try {
+            await handle(req, res);
+          } catch (err) {
+            console.error("Error occurred handling", req.url, err);
+            res.statusCode = 500;
+            res.end("internal server error");
+          }
         }
-      }
-    ).listen(port, () => {
-      console.log(`> Ready on https://${hostname}:${port}`);
-    });
+      ).listen(port, () => {
+        console.log(`> Ready on https://${hostname}:${port}`);
+      });
+    } catch (e) {
+      (e) => console.error(e);
+    }
   })
   .catch((e) => console.error(e));
