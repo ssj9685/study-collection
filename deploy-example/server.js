5c5cea4e0e8b4f81feeb49a9c8cc054df12fdbca
신석진( Seokjin Shin) <ssj9685@naver.com>
2024-07-07 18:05:14 +0900

    nextjs custom server https

---
+++ deploy-example/server.js
@@ -0,0 +1,26 @@
+import {createServer} from 'https';
+import next from next;
+
+const hostname = "https-test.p-e.kr";
+const port = 443;
+
+const app = next({ hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer({
+    key: '/etc/letsencrypt/live/https-test.p-e.kr/privkey.pem',
+    cert: '/etc/letsencrypt/live/https-test.p-e.kr/cert.pem',
+  }, async (req, res) => {
+    try {
+        await handle(req, res);
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  })
+    .listen(port, () => {
+      console.log(`> Ready on https://${hostname}:${port}`);
+    });
+});
